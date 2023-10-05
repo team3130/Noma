bd80c466c973efc6dd42448c9b06b2fb92570966
Robot1235369 <73447037+Robot1235369@users.noreply.github.com>
2023-10-05 01:22:23 +0000

    forgot to check if it could build (it couldn't)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
@@ -15,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.RotaryArm;
+import frc.robot.commands.ToggleRotary;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
