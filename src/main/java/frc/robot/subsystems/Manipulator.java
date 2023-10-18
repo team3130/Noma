d59311ccbdced0fdc94140fca69966bfc0eb76bb
Robot1235369 <IsaacCA2018@gmail.com>
2023-10-18 01:22:36 +0000

    we tried

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -4,29 +4,42 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.function.DoubleSupplier;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import frc.robot.Constants;
 
 public class Manipulator extends SubsystemBase {
 
-  private final WPI_TalonFX m_manipulatorMotor; // we should probably change these names once we learn more
+    private double motorSpeed;
+    private final WPI_TalonFX m_manipulatorMotor; // we should probably change these names once we learn more
 
 
   public Manipulator() {
     m_manipulatorMotor = new WPI_TalonFX(Constants.Manipulator.CAN_manipulatorMotor);
     m_manipulatorMotor.configFactoryDefault();
     m_manipulatorMotor.setInverted(false);
+    motorSpeed = 0.0;
   }
 
   public void setSpeed(double x) {
+    motorSpeed = x;
     m_manipulatorMotor.set(x);
+  }
+  public double getSpeed() {
+    return motorSpeed;
   }
 
   @Override
-  public void periodic() {
+  public void periodic() {}
 
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    builder.addDoubleProperty("speed", this::getSpeed, this::setSpeed);
   }
 
   @Override
